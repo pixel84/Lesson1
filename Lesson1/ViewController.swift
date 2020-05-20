aa545611104f07b5c663aabd42d2d83fec3e315f
Alex Shader <pixel84@gmail.com>
2020-05-20 12:08:45 +0300

    lesson 1 complete

--- Lesson1/ViewController.swift
+++ Lesson1/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleView: UILabel!
+    @IBOutlet weak var loginTitleView: UILabel!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordTitleView: UILabel!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleView.text = "Погода"
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
-
+    
+    @IBAction func loginPressed() {
+        let login = loginField.text!
+        let password = passwordField.text!
+        if login == "admin" && password == "123456" {
+            print("Access Granted")
+        } else {
+            print("Access Denied")
+        }
+    }
+    @objc func keyboardUp(notification: Notification) {
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        scrollBottomConstraint.constant = frame.height
+        
+    }
+    @objc func keyboardDown(notification: Notification) {
+        scrollBottomConstraint.constant = 0
+        
+    }
+    
 }
 
